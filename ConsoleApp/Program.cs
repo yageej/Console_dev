091173cd83e4de602d00cc671f1da46d293d6d55
yageej <greekgodstud2@gmail.com>
2024-02-18 02:02:37 +0800

    3rd commit test

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,12 +7,13 @@ namespace ConsoleApp{
     public class Program{
         static void Main(string[] args){
 
-        //  test second Commit
+        //  testing third commit
 
         int i = 0;
+        string[] arr1 = {"Pep","Gee","Que"};
 
-        while(i < 5){
-            Console.WriteLine(i);
+        while(i < arr1.Length){
+            Console.WriteLine(arr1[i]);
             i++;
         }
 
