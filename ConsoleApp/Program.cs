15dc63fedb74893d1316506ef2bfe8e85ed3a395
yageej <greekgodstud2@gmail.com>
2024-02-18 01:44:24 +0800

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+namespace ConsoleApp{
+
+    public class Program{
+        static void Main(string[] args){
+            Console.WriteLine("TESTING");
+
+        }
+    }
+
+}
