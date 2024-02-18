2d3208b178df82b9039d8c131bd567d37158cad5
yageej <greekgodstud2@gmail.com>
2024-02-18 15:57:25 +0800

    Adding Quiz game w/Lives Console

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,15 +7,40 @@ namespace ConsoleApp{
     public class Program{
         static void Main(string[] args){
 
-        //  testing commit on dev_test branch
+        // Quiz Game w/ Lives recap
 
-        int i = 0;
-        string[] arr1 = {"Pep","Gee","Que"};
+        int u_life = 5;
 
-        while(i < arr1.Length){
-            Console.WriteLine(arr1[i]);
-            i++;
+
+        while(u_life > 0)
+        {
+            Console.WriteLine("Life remaining: "+u_life);
+            Console.WriteLine("Who is the strongest Anime Protagonist?");
+            Console.WriteLine("Who is the strongest Anime Protagonist?");
+            string answer = Console.ReadLine();
+            if(answer.Equals("goku",StringComparison.InvariantCultureIgnoreCase))
+                {
+
+                    Console.WriteLine("You are correct!" + " The strongest Character is always "+answer);
+                    break;
+                }
+            else if(u_life >= 1)
+                {
+                    Console.WriteLine("INCORRECT! Guess again!");
+                    u_life--;
+                }
+             else
+                {
+                    Console.WriteLine("INCORRECT!");
+                    u_life--;
+                }
+                       
+            if(u_life == 0){
+                Console.WriteLine("Game over");
+            }
         }
+
+
 
         }
     }
