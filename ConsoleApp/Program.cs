8150c09ba9f6078ad6a75adfe683ca44e3f6a9b1
yageej <greekgodstud2@gmail.com>
2024-02-18 16:33:17 +0800

    Using For Loop

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,44 +5,64 @@
 namespace ConsoleApp{
 
     public class Program{
-        static void Main(string[] args){
-
-        // Quiz Game w/ Lives recap
-
-        int u_life = 5;
-
-
-        while(u_life > 0)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Life remaining: "+u_life);
-            Console.WriteLine("Who is the strongest Anime Protagonist?");
-            Console.WriteLine("Who is the strongest Anime Protagonist?");
-            string answer = Console.ReadLine();
-            if(answer.Equals("goku",StringComparison.InvariantCultureIgnoreCase))
-                {
 
-                    Console.WriteLine("You are correct!" + " The strongest Character is always "+answer);
-                    break;
+            string[] user = {"geje","quen","pep"};
+            string[] pw = {"212121","141414","0404"};            
+            bool found = false;
+            Console.WriteLine("Enter your UserName: ");
+            string user_answer = Console.ReadLine();
+
+            Console.WriteLine("Enter your Password: ");
+            string pw_answer = Console.ReadLine();
+
+            for (int index = 0; index < user.Length; index++)
+            {
+                if(user_answer.Equals(user[index]) && pw_answer.Equals(pw[index])){
+                    found = true;
+                    Console.WriteLine($"Welcome! {user[index]}");
                 }
-            else if(u_life >= 1)
-                {
-                    Console.WriteLine("INCORRECT! Guess again!");
-                    u_life--;
+
+                if(!found){
+                    Console.WriteLine("User not found!");
                 }
-             else
-                {
-                    Console.WriteLine("INCORRECT!");
-                    u_life--;
-                }
-                       
-            if(u_life == 0){
-                Console.WriteLine("Game over");
+
             }
+
+
         }
 
-
-
-        }
+        
     }
 
 }
+
+        // Quiz Game w/ Lives recap
+
+        // int u_life = 5;
+
+
+        // while(u_life > 0)
+        // {
+        //     Console.WriteLine("Life remaining: "+u_life);
+        //     Console.WriteLine("Who is the strongest Anime Protagonist?");
+        //     Console.WriteLine("Who is the strongest Anime Protagonist?");
+        //     string answer = Console.ReadLine();
+        //     if(answer.Equals("goku",StringComparison.InvariantCultureIgnoreCase))
+        //         {
+
+        //             Console.WriteLine("You are correct!" + " The strongest Character is always "+answer);
+        //             break;
+        //         }
+        //      else
+        //         {
+        //             Console.WriteLine("INCORRECT!");
+        //             u_life--;
+        //         }
+                       
+        //     if(u_life == 0){
+        //         Console.WriteLine("Game over");
+        //     }
+        // }
+
