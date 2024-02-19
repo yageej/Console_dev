d00b6dd8fc1279c19e2701463d5add6f7c65b528
yageej <greekgodstud2@gmail.com>
2024-02-19 20:47:24 +0800

    classes and modifiers

--- ConsoleApp/Arithmetic.cs
+++ ConsoleApp/Arithmetic.cs
@@ -1,16 +1,16 @@
 ﻿namespace ConsoleApp;
 
-public class Arithmetic
+public static class Arithmetic         // can instantiate if using static modifier
 {
 
     // Method inside this class 
     //doenst need static
-    public int sumOfNumbers(int n1, int n2, int n3)
+    public static int sumOfNumbers(int n1, int n2, int n3)
     {
         return n1 + n2 + n3;
     }
 
-   public int DiffOfNumbers(int n1, int n2, int n3)
+   public static int DiffOfNumbers(int n1, int n2, int n3)
     {
         return n1 - n2 - n3;
     }
