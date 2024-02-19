c69ac0a7504ff38d4267fae4f74ba2ffd8ae4bac
yageej <greekgodstud2@gmail.com>
2024-02-20 06:16:57 +0800

    COnstructors wit parameters passed

---
+++ ConsoleApp/Student.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp;
+
+public class Student
+{
+
+    //class attributes
+    public string fname;
+    public string lname;
+    public int grade;
+    public char section;
+
+
+    // constructor should be no returntype like void
+    public Student(string firstname, string lastname, int grade, char section)
+    { 
+   
+    //  this.variable = parameter
+
+        this.fname = firstname;
+        this.lname = lastname;
+        this.grade = grade;
+        this.section = section;
+
+        Console.WriteLine("Student Created");
+        Console.WriteLine(firstname + " " + lastname);
+        Console.WriteLine(grade+ " "+ section);
+
+
+    }
+
+}
