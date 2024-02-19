b1ff89a9c017c74e7ff7d9d605b9f8622eaf6d56
yageej <greekgodstud2@gmail.com>
2024-02-20 06:07:31 +0800

    Using this and constructors

---
+++ ConsoleApp/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp;
+
+public class Product
+{
+
+    //Class attribute
+    public string name;
+    public float price;
+
+
+    // Constructor sample with parameter
+    public Product(string n1, float f1)
+    {
+        this.name = n1;
+        this.price = f1;
+    }
+
+
+
+
+
+}
