3f0370161182f49f12b028cf387265c08ba17ecf
yageej <greekgodstud2@gmail.com>
2024-02-19 20:56:54 +0800

    classes and modifiers

--- ConsoleApp/Constants.cs
+++ ConsoleApp/Constants.cs
@@ -3,6 +3,6 @@
 public class Constants   // its okay if class is not static but its contents should be static
 {
     // never changing value variables
-    public static float PI = 3.14f;
-    public static float gravity = 9.8f;
+    public static readonly float PI = 3.14f;
+    public static readonly float gravity = 9.8f;
 }
