ec84073d5c9aff516eaff3f33aee3364bd423611
yageej <greekgodstud2@gmail.com>
2024-02-19 20:41:39 +0800

    classes and modifiers

---
+++ ConsoleApp/Arithmetic.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp;
+
+public class Arithmetic
+{
+
+    // Method inside this class 
+    //doenst need static
+    public int sumOfNumbers(int n1, int n2, int n3)
+    {
+        return n1 + n2 + n3;
+    }
+
+   public int DiffOfNumbers(int n1, int n2, int n3)
+    {
+        return n1 - n2 - n3;
+    }
+}
