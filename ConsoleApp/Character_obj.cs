aa22313bc8ceb97623271301bf921fb75a47fb73
yageej <greekgodstud2@gmail.com>
2024-02-20 15:28:45 +0800

    object methods

---
+++ ConsoleApp/Character_obj.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+
+namespace ConsoleApp;
+
+public class Character_obj
+{
+    //attribute
+    public string name, dialog;
+    public int hp,mp,lvl;
+
+    public Character_obj(string name, string dialog,int hp)
+    {
+        //this.variable = parameter
+        this.name = name;
+        this.dialog = dialog;
+        this.hp = hp;
+
+
+
+    }
+
+    // sample of obj method
+    public void CharObjMet_greet()
+    {
+        Console.WriteLine("Hi "+name);
+
+        
+    }
+    // sample of obj method
+    public string CharObjectMet_dialog(){
+        return "Voiceline: " +dialog;
+    }
+   
+
+}
