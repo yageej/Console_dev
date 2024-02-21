10909bb2c84822d6744c808de6a4877dcb0d460d
yageej <greekgodstud2@gmail.com>
2024-02-21 20:38:18 +0800

    OVerloading constructors

--- ConsoleApp/Product.cs
+++ ConsoleApp/Product.cs
@@ -2,21 +2,41 @@
 
 public class Product
 {
+    //attributes
+    // automatic property creation
+    public int productId{get;set;}
+    public string productName{get;set;}
+    public float productPrice{get; set;}
 
-    //Class attribute
-    public string name;
-    public float price;
 
-
-    // Constructor sample with parameter
-    public Product(string n1, float f1)
+    // Overloading Constructor sample with parameter or 
+    //multiple constructor with same name but diff number of parameter/arguments
+    public Product(int productId, string productName)
     {
-        this.name = n1;
-        this.price = f1;
+        this.productId = productId;
+        this.productName = productName;
     }
 
+    public Product(int productId, string productName, float productPrice)
+    {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+            Console.WriteLine("Proudct ID: "+productId);
+            Console.WriteLine("Proudct Name: "+productName);
+            Console.WriteLine("Proudct Price: "+productPrice);
 
+    }
 
+    public Product(){ // no parameter
+
+    }
+    // public void printProduct(int n1, string p1, float p2){
+        
+    //         Console.WriteLine("Proudct ID: "+productId);
+    //         Console.WriteLine("Proudct Name: "+productName);
+    //         Console.WriteLine("Proudct Price: "+productPrice);
+    // }
 
 
 }
