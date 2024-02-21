6f4fe539c664a0f6b31c51d89b29116dd7efee93
yageej <greekgodstud2@gmail.com>
2024-02-21 21:52:06 +0800

    Inheritance - BASE and DERIVE CLASSES

--- ConsoleApp/Person.cs
+++ ConsoleApp/Person.cs
@@ -2,11 +2,22 @@
 
 public class Person
 {
+    // This is will be the BASE CLASS
     //Attributes
-    public string fname;
-    public string lname;
-    public char sex;
-    public int age;
+    public string fname{get; set;}
+    public string lname{get; set;}
+    public char sex{get; set;}
+    public int age{get; set;}
 
+    // Base Class w Constructor w parameter 
+    public Person(string fname, string lname){
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    // Base Method
+    public void introduceSelf(){
+    
+    }
 
 }
